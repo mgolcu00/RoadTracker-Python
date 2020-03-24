a6d5a1c759c4babca316295eb17f45bf0aa184f2
mgolcu <mgolcu00@gmail.com>
2020-03-24 22:10:00 +0300

    add modes

--- README.md
+++ README.md
@@ -7,8 +7,10 @@
  - Videos fomrat  must be mp4 (mp4v) 
 
 ## Usage 
- - ```python main.py -v <VideoPath> -s <SavePath> ```
- - for example ```python main.py -v videos/test.mp4 -s result/result.mp4 ```
+ - ```python main.py -v <VideoPath> -s <SavePath> -m <mode>```
+ - modes 1 -> just lines
+ - modes 2 -> lines and road try
+ - for example ```python main.py -v videos/test.mp4 -s result/result.mp4 -m 1```
 
 ##### Description 
 Bu program geliştirilme aşamasında olduğu için herhangi hata ve kullanım sıkıntısında belirtiniz.
--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 # python main.py -v <VideoPath> -s <SavePath>
 # VideoPath = the name or path of the mp4 video file you want to use / kullanmak istediğiniz mp4 video dosyasının adı yada yolu
 # SavePath = file name and path where the processed and drawn path video will be saved / işlenen ve çizilen yol videosunun kaydedileceği dosya ismi ve yolu
-# 
+#
 # tr / örnek kullanım şekli -> python main.py -v videos/test.mp4 -s result/result.mp4
-# 
+#
 
-
+import src.still_processing as sp
 import src.imageProcess as imageProcess
+import src.line_detector as ld
 import sys
 import getopt
 import matplotlib.pyplot as plt
@@ -21,21 +22,24 @@ import os
 # -s save path
 videoPath = ''
 savePath = ''
+mode = ''
 argss = sys.argv[1:]
 
 try:
-    opts, args = getopt.getopt(argss, "hv:s:", ["vpath=", "spath="])
+    opts, args = getopt.getopt(argss, "hv:s:m:", ["vpath=", "spath=", "mode="])
 except getopt.GetoptError:
-    print('main.py -v <VideoPath> -s <SavePath>')
+    print('main.py -v <VideoPath> -s <SavePath> -m <mode>')
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print('main.py -v <VideoPath> -s <SavePath>')
+        print('main.py -v <VideoPath> -s <SavePath> -m <mode>')
         sys.exit()
     elif opt in ("-v", "--vpath"):
         videoPath = arg
     elif opt in ("-s", "--spath"):
         savePath = arg
+    elif opt in ("-m", "--mode"):
+        mode = arg
 
 if savePath == '':
     savePath = 'result/'
@@ -68,6 +72,60 @@ def saveVideo(cap, spath):
     cv2.destroyAllWindows()
 
 
+def saveVideo2(cap, spath):
+    forucc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(spath,
+                          forucc, 60.0, (int(cap.get(3)), int(cap.get(4))))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            # frame = cv2.flip(frame,1,dst=none) #frame cevirmek icin
+            res = sp.process_image(frame)
+            out.write(res)
+            cv2.imshow('result', res)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+def saveVideo3(cap, spath):
+    forucc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(spath,
+                          forucc, 60.0, (int(cap.get(3)), int(cap.get(4))))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            # frame = cv2.flip(frame,1,dst=none) #frame cevirmek icin
+            res = ld.find_street_lanes(frame)
+            out.write(res)
+            cv2.imshow('result', res)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
 #c = getVideo(imageProcess.set.testdeo)
-print(videoPath + '\n'+savePath)
-saveVideo(getVideo(videoPath), savePath)
+# print(videoPath + '\n'+savePath)
+# print(mode)
+
+vid = getVideo(videoPath)
+
+
+if mode == '1':
+    saveVideo(vid, savePath)
+elif mode == '2':
+    saveVideo2(vid, savePath)
+else:
+    print('mode error try (1 or 2)')
